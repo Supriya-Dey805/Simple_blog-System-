16df555e6844c7661eaeb7f8698d4b32528209f5
Supriya-Dey805 <supriya.dey@bsccmh.christuniversity.in>
2026-05-18 21:23:45 +0530

    api changes

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -122,3 +122,5 @@ const CreatePost = () => {
 };
 
 export default CreatePost;
+
+//814118 
--- src/services/api.js
+++ src/services/api.js
@@ -4,16 +4,13 @@ const API = axios.create({
   baseURL: "http://localhost:5000/api"
 });
 
-export const deletePost = (postId) => {
-  return API.delete(`/posts/${postId}`);
-};
+// DELETE POST
+export const deletePost = (postId) => API.delete(`/posts/${postId}`);
 
-export const updatePost = (postId, postData) => {
-  return API.put(`/posts/${postId}`, postData);
-};
+// UPDATE POST
+export const updatePost = (postId, postData) => API.put(`/posts/${postId}`, postData);
 
-export const likePost = (postId) => {
-  return API.put(`/posts/like/${postId}`);
-};
+// LIKE POST
+export const likePost = (postId) => API.put(`/posts/like/${postId}`);
 
 export default API;
